8e60b7cbbed7b3ad4da8cd4039c9ac322e4eccae
bradyhibbard <38871433+bradyhibbard@users.noreply.github.com>
2024-09-18 08:18:38 -0600

    changed GH_TOKEN to GITHUB_TOKEN in automation actions.

--- obj/Debug/net8.0-windows/Winton.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Winton.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Winton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf550e04b3020efc6c8f87559bc4aa865c6f69e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4525260c711b63c59c4cee02a9707463a0735b9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Winton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Winton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
